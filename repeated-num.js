c1d0eda7dc039ca4861237544f34b00cfacd56d4
ashraful-fahim <ashraful30june@gmail.com>
2025-02-16 20:44:08 +0600

    changed the logic a bit

--- repeated-num.js
+++ repeated-num.js
@@ -9,12 +9,17 @@ find: 5
 const numbers = [5, 6, 11, 12, 98, 5, 234, 56, 32, 4, 5, 5, 5, 5, 5, 24];
 const need = 5;
 const unit = [];
-for (num of numbers) {
-    if (num === need) {
-        unit.push(num);
+if (numbers.includes(need)){
+    for (num of numbers) {
+        if (num === need) {
+            unit.push(num);
+        }
     }
+    const len = unit.length;
+    console.log('The number is repeated', len, 'times.');    
 }
-const len = unit.length;
-console.log('The number is repeated', len, 'times');
+else{
+    console.log('The number is not in the array.')
+}
 
 
